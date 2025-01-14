f4d2b059e83b617cf179503c1686fca4889740c2
Vaishnaviwarathe16 <warathevaishnavi2004@gmail.com>
2025-01-14 12:35:14 +0530

    initial commit

--- vege/models.py
+++ vege/models.py
@@ -24,7 +24,10 @@ class StudentID(models.Model):
 
     def __str__(self) -> str: 
         return self.student_id
-    
+
+
+class Subject(models.Model):
+    subject_name = models.CharField(max_length=100)
 
 class Student(models.Model):
     department = models.ForeignKey(Department, related_name="depart", on_delete=models.CASCADE)
@@ -40,3 +43,16 @@ class Student(models.Model):
     class Meta:
         ordering = ['student_name']
         verbose_name = 'student'
+
+
+
+class SubjectMarks(models.Model):
+    student = models.ForeignKey(Student, related_name="studentmarks", on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    marks = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f'{self.student.student_name}{self.subject.subject_name}'
+    
+    class Meta:
+        unique_together = ['student', 'subject']
